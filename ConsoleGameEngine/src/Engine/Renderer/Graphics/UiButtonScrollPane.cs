e4b905874089077835de9d3efe41adee40b43beb
ML6200 <78232881+ML6200@users.noreply.github.com>
2025-12-08 16:31:31 +0100

    Scroll pane for more advanced ui (not finished)

---
+++ ConsoleGameEngine/src/Engine/Renderer/Graphics/UiButtonScrollPane.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using ConsoleGameEngine.Engine.Input;
+
+namespace ConsoleGameEngine.Engine.Renderer.Graphics;
+
+public class UiButtonScrollPane : UiPanel, IFocusable
+    
+{
+    private string _text = "";
+
+    public List<UiButton> Buttons;
+    public bool IsFocused { get; set; }
+    public bool CanFocus { get; set; } = true;
+    
+    public bool HasBorder { get; set; } = false;
+
+    public event EventHandler OnClick;
+    
+    public ConsoleColor FocusedBgColor { get; set; } = ConsoleColor.DarkBlue;
+    
+    public UiButtonScrollPane(string text)
+    {
+        Buttons = new List<UiButton>();
+    }
+
+    public UiButtonScrollPane()
+    {
+        Buttons = new List<UiButton>();
+    }
+
+
+    public void OnFocusGained()
+    {
+        HasBorder = true;
+    }
+
+    public void OnFocusLost()
+    {
+        HasBorder = false;
+    }
+
+    public void OnFocusActivate(KeyEventArgs? param)
+    {
+        int offset = 0;
+        foreach (var child in Children)
+        {
+            var childWordPos = child.WorldPosition + offset;
+        }
+    }
+}
