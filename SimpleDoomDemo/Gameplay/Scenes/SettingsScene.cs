14ead035b0023ec80c858cffc31a47c43b7210bb
ML6200 <78232881+ML6200@users.noreply.github.com>
2025-12-09 19:54:06 +0100

    Added settings placeholder

---
+++ SimpleDoomDemo/Gameplay/Scenes/SettingsScene.cs
@@ -0,0 +1,26 @@
+using ConsoleGameEngine.Engine;
+
+namespace SimpleDoomDemo.Gameplay.Scenes;
+
+public class SettingsScene : IGameScene
+{
+    public void Initialize(ConsoleEngine consoleEngine)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUpdate(double deltaTime)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnExit()
+    {
+        throw new System.NotImplementedException();
+    }
+}
