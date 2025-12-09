cd1a5ddc872081a01f2862f1d643121a3d820533
ML6200 <78232881+ML6200@users.noreply.github.com>
2025-12-09 23:30:44 +0100

    Created initial manager for the game + moved DGS to scenes

---
+++ SimpleDoomDemo/Gameplay/DoomGameManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Text;
+using System.Threading;
+using ConsoleGameEngine.Engine;
+using SimpleDoomDemo.Gameplay.Scenes;
+
+namespace SimpleDoomDemo.Gameplay;
+
+public class DoomGameManager
+{
+    public string DefaultMapPath { get; set; }
+    public static bool IsTipShown = false;
+    private ConsoleEngine _engine;
+    private MainMenuScene _menuScene;
+
+    public DoomGameManager(string defaultMapPath)
+    {
+        DefaultMapPath = defaultMapPath;
+        Console.OutputEncoding = Encoding.UTF8;
+        _engine= new ConsoleEngine();
+        _engine.TargetUpdatesPerSecond = 40;  // Game logic updates at 40 FPS
+        _engine.TargetRenderFPS = 100;       // Rendering at 100 FPS
+        _engine.Initialize();
+        _menuScene = new MainMenuScene(DefaultMapPath);
+    }
+
+    public void StartGame()
+    {
+        _engine.SetInitialScene(_menuScene);
+        _engine.OnStart();
+        
+        while (_engine.IsRunning)
+        {
+            Thread.Sleep(100);
+        }
+        
+        _engine.Dispose();
+    }
+}
